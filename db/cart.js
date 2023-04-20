44752ce68148732e2f83e3990c746977589dd132
Sarah <sarah.celiann@gmail.com>
2023-04-20 10:55:18 -0500

    started addItemToCart

--- db/cart.js
+++ db/cart.js
@@ -1,9 +1,27 @@
 const client = require('./client')
 
 //Add item to cart
-const addItemToCart = async (spellId, adventurerId) => {
+const addItemToCart = async (spellId, adventurerId, quantity = 1) => {
+try{
+    const {rows: [cart]} = await client.query(`
+        INSERT INTO cart ("adventurerId", "spellId", quantity)
+        VALUES ($1, $2, $3)
+        RETURNING *;
+    `, [adventurerId, spellId, quantity]);
+
+    console.log(cart);
+    return cart;
+} catch(error) {
+    throw error;
+}
 }
 
+// const test = async() =>{
+//   client.connect()
+//   console.log(await addItemToCart(1, 1, 1));
+//   client.end()
+// }
+// test()
 //get cart by adventurer id
 
 const getCartByAdventurerId = async (adventurerId) => {
--- tests/cart.test.js
+++ tests/cart.test.js
@@ -1,15 +1,19 @@
 const { addItemToCart, getCartByAdventurerId, updateCartQuanity, setCartInactive } = require('../db/cart');
+const client = require('../db/client');
 
 //addItemToCart
 //expect new item to be added to cart
 // test('adds 1 + 2 to equal 3', () => {
 //     expect(sum(1, 2)).toBe(3);
 //   });
+client.connect();
 
-test('expect new item to be added to cart', () => {
+test('expect new item to be added to cart', async () => {
     const spellId = 7;
-    expect(addItemToCart(spellId)).toStrictEqual({
-        id: 1,
+    const adventurerId = 1;
+    const response = await addItemToCart(spellId, adventurerId);
+    expect(response).toStrictEqual({
+        id: 4,
         adventurerId: 1,
         spellId: 7,
         quantity: 1,
@@ -37,7 +41,7 @@ test('return cart based on adventurerId', () => {
 test('return cart with updated spell quantity', () => {
     const spellId = 7;
     const newQuantity = 3;
-    expect(updateCartQuanity(spellId, newQuantity).toStrictEqual({
+    expect(updateCartQuantity(spellId, newQuantity).toStrictEqual({
         id: 1,
         adventurerId: 1,
         spellId: spellId,
@@ -56,5 +60,7 @@ test('return cart with status inactive', () => {
         spellId: 7,
         quantity: 1,
         isActive: false
-    }))
+    }));
 })
+
+client.end();
