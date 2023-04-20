ceb78f1445499e50d0f08b5dba17e720f8109b07
zachary bordelon <zackmbordelon@gmail.com>
2023-04-20 10:38:32 -0500

    userDummy74

---
+++ src/Components/users.js
@@ -0,0 +1,32 @@
+import React, { useState} from "react";
+
+
+user = () => {
+    return(
+        <div>
+            <span>Username Displays HERE</span>
+            <span>(If admin, badge will display HERE)</span>
+            <div>
+                <ol>
+                    <li>
+                        I AM A PREVIOUS ORDER
+                    </li>
+                    <li>
+                        I AM A PREVIOUS ORDER
+                    </li>
+                    <li>
+                        I AM A PREVIOUS ORDER
+                    </li>
+                    <li>
+                        I AM A PREVIOUS ORDER
+                    </li>
+                    <li>
+                        I AM A PREVIOUS ORDER
+                    </li>
+                </ol>
+            </div>
+        </div>
+    )
+}
+
+export default user
