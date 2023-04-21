92c75a2e56808db6569b563ce0f7875b6d7e7455
zachary bordelon <zackmbordelon@gmail.com>
2023-04-21 17:08:42 -0500

    dummy log in component

--- src/Components/logIn.js
+++ src/Components/logIn.js
@@ -1,1 +1,13 @@
 import React, { useState } from 'react' 
+
+const logIn = () => {
+    return(
+        <div>
+            <form id="logInForm">
+                <input id="usernameInput" placeholder="username"></input>
+                <input id="passwordInput" palceholder="password"></input>
+                <button id="logInButton"> Log In </button>
+            </form>
+        </div>
+    )
+}
