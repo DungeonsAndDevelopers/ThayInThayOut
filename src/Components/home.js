318bf644f152143ff55d0df1bac62ecf3641753f
Sarah <sarah.celiann@gmail.com>
2023-04-22 12:20:40 -0500

    skeleton created

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,1 +1,31 @@
 import React, { useState } from 'react' 
+
+const BannerAd = () => {
+    return (
+        <h1>Banner Image Goes Here</h1>
+    );
+}
+
+const PopularSpells = () => {
+    return (
+        <h1>Popular Spells Go Here</h1>
+    );
+};
+
+const SpellSchools = () => {
+    return (
+        <h1>Spell Schools Go Here</h1>
+    );
+};
+
+const Home = () => {
+    return (
+        <div>
+            <BannerAd />
+            <PopularSpells />
+            <SpellSchools />
+        </div>
+    );
+};
+
+export default Home;
