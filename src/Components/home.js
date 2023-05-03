d1596ad870aa8bae3c18f83a2123b1da064d1ea6
Sarah <sarah.celiann@gmail.com>
2023-05-03 17:39:41 -0500

    added images to popular spells

--- src/Components/home.js
+++ src/Components/home.js
@@ -13,21 +13,22 @@ const BannerAd = () => {
 
 const PopularSpells = (props) => {
     const [spells, setSpells] = useState([]);
+    const imagesArray = ["/images/findtraps.png", "/images/foresight.png", "/images/identify.png", "/images/telepathicbond.png", "/images/tongues.png", "/images/trueseeing.png"];
     useEffect(() => {
         fetchAllSpellsBySchool(props.school, setSpells);
     }, []);
     return (
         <div id="popular-spells">
         <h2>Popular Spells</h2>
-        {spells.map((spell => {
+        {spells.map((spell, index) => {
             return (
                 <div key={spell.id} className='single-spell'>
-                    <img src='https://picsum.photos/200'></img>
+                    <img style={{height: 200, width: 200}}src={imagesArray[index]}></img>
                     <p className='spell-name'>{spell.name}  <em>500gp</em></p>
                     <button>Add To Cart</button>
                 </div>
             )
-        })) }
+        }) }
         </div>
     );
 };
