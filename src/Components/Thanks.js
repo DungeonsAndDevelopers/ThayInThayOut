373255d0816f5798f91af6caa44907972ab9ac50
Tai Truong <tylertruong2902@gmail.com>
2023-05-04 10:33:11 -0600

    add pics

--- src/Components/Thanks.js
+++ src/Components/Thanks.js
@@ -3,11 +3,16 @@ import React from "react";
 const Thanks = () =>{
 
   return(
-    <di>
-      <h3>Thanks for shopping</h3>
-      <h4>Stand Back!</h4>
+    <>
+      <div class="tagForThanks">Thank You For Shopping</div>
+
+      <div class="tagName">Stand Back!</div>
+
       <h4 className="text-danger" >Caution: Do not look directly into the portal</h4>
-    </di>
+      
+      <img src="../images/Thanks.png" alt="thanks" class="thankyou1"/>
+
+    </>
   )
 }
 
