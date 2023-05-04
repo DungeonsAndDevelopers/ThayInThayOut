cb79cabbe8612a88fb06cfe39aff1c7906ead6ce
Justin Keowen <jkeowen@gmail.com>
2023-05-03 20:06:00 -0500

    cart and checkout page tweeks

--- src/Components/cart.js
+++ src/Components/cart.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react' 
+import React, { useState, useEffect } from 'react'; 
 import fetchAdventurerCart from '../AjaxHelpers/Cart';
 import { Link, useNavigate } from 'react-router-dom';
 
@@ -6,27 +6,36 @@ import { Link, useNavigate } from 'react-router-dom';
 const Cart = () => {
 	const [ cart, setCart ] = useState([]);
 
+	const navigate = useNavigate();
+
 	useEffect(()=>{
 		fetchAdventurerCart(window.localStorage.getItem('username'), setCart);
-	},[])
+	},[]);
+
+
+	const navigateToCheckout = ()=>{
+		navigate('/checkout');
+	}
+
+
 
 	return (
-		<>
+		<div className='d-flex justify-content-center'>
 		{
 		window.localStorage.getItem('advToken') ?
-		<div id="cart" className='p-5 d-flex justify-content-center fixed-parent'>
-			<div className='bg-darkish-cream rounded width-fit-content p-2 pb-1 mr-5 '>
+		<div id="cart" className='p-5 d-flex justify-content-center ml-5 w-75'>
+			<div className='bg-darkish-cream rounded width-fit-content p-2 pb-1 mr-5 w-50'>
 				{
 					cart.map((item, index)=>{
 						return(
 							item.is_active ? 
-							<div key={index} className='rounded d-flex justify-content-between  p-3 mb-2 bg-ivory shadow-lg'>
+							<div key={index} className='rounded d-flex justify-content-between  p-3 mb-2 bg-ivory shadow-lg '>
 								<div className='mr-5'>Image Place Holder</div>
 								<div>
 									<div className='mb-2'>{item.name}</div>
-									<div className='mb-2'> quanity: {item.quanity}</div>
+									<div className='mb-2'> quantity: {item.quantity}</div>
 									<div className='mb-2' >price: {50 + (100 * item.base_level)}</div>
-									<button className='px-2 rounded'>Remove</button>
+									<button className='px-2 rounded red-shadow'  >Remove</button>
 								</div>
 							</div>: null
 						)
@@ -46,12 +55,12 @@ const Cart = () => {
 						}gp
 						</h3>
 					</div>
-					<button className="rounded px-2">Checkout</button>
+					<button className="rounded px-2 red-shadow" onClick={navigateToCheckout} >Checkout</button>
 				</div>
 			</div>	
 		</div> : <h2 style={{textAlign: "center", padding: '5%'}}>You must <Link to="/login">login</Link> to continue</h2>
 					}
-					</>
+					</div>
 	)
 }
 
